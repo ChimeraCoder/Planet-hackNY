b19106978d9b568272cb056d856c9429576358fa
Jennifer Rubinovitz <jrubinovitz@gmail.com>
2013-05-30 13:26:46 -0400

    rendering the fellows2013 template. still need: school and startup url

--- server.go
+++ server.go
@@ -240,7 +240,6 @@ func serveAbout(w http.ResponseWriter, r *http.Request) {
 }
 
 func serveFellows(w http.ResponseWriter, r *http.Request) {
-
 	var fellows []Fellow
 	if err := withCollection(FELLOWS_DB, func(c *mgo.Collection) error {
 		return c.Find(bson.M{"year": 2013}).All(&fellows)
@@ -248,7 +247,7 @@ func serveFellows(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	s1, err := template.ParseFiles("templates/fellowsbase.tmpl", "templates/fellows2013.tmpl")
+s1, err := template.ParseFiles("templates/fellowsbase.tmpl", "templates/fellows2013.tmpl")
 	if err != nil {
 		panic(err)
 	}
